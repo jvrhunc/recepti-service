043b4b78c08b3bf8a3e6249d4bf5142e44a54e2a
Jernej Vrhunc <jv4739@student.uni-lj.si>
2020-12-01 10:26:15 +0100

    Popravljen path, spremenjena logika za Sestavine in Recepte (združeno v en endpoint).

--- src/main/java/si/fri/rso/recepti/repositories/SestavineRepository.java
+++ src/main/java/si/fri/rso/recepti/repositories/SestavineRepository.java
@@ -1,8 +1,12 @@
 package si.fri.rso.recepti.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import si.fri.rso.recepti.models.Recept;
 import si.fri.rso.recepti.models.Sestavina;
+
+import java.util.List;
 
 public interface SestavineRepository extends JpaRepository<Sestavina, Integer> {
 
+    List<Sestavina> getByRecept(Recept recept);
 }
--- src/main/java/si/fri/rso/recepti/resources/ReceptiResource.java
+++ src/main/java/si/fri/rso/recepti/resources/ReceptiResource.java
@@ -8,18 +8,22 @@ import si.fri.rso.recepti.models.Recept;
 import si.fri.rso.recepti.models.Sestavina;
 import si.fri.rso.recepti.models.enums.Tip;
 import si.fri.rso.recepti.repositories.ReceptiRepository;
+import si.fri.rso.recepti.repositories.SestavineRepository;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/recepti")
+@RequestMapping("/v1/recepti")
 @RefreshScope
 public class ReceptiResource {
 
     @Autowired
     private ReceptiRepository receptiRepository;
+
+    @Autowired
+    private SestavineRepository sestavineRepository;
 
     @Value("${my.greeting:test}")
     private String greeting;
@@ -68,6 +72,47 @@ public class ReceptiResource {
         }).orElseGet(() -> {
             recept.setReceptId(receptId);
             return receptiRepository.save(recept);
+        });
+    }
+
+    @GetMapping("/{receptId}/sestavine")
+    public List<Sestavina> getSestavineByRecept(@PathVariable("receptId") Integer receptId) {
+        Optional<Recept> recept = receptiRepository.findById(receptId);
+        return recept.map(value -> sestavineRepository.getByRecept(value)).orElse(null);
+    }
+
+    @PostMapping("/{receptId}/sestavine/add")
+    public Sestavina addSestavina(@PathVariable("receptId") Integer receptId, @RequestBody Sestavina sestavina) {
+        Optional<Recept> recept = receptiRepository.findById(receptId);
+        if (recept.isPresent()) {
+            sestavina.setRecept(recept.get());
+            sestavina.setCreated(LocalDate.now());
+            return sestavineRepository.save(sestavina);
+        } else {
+            return null;
+        }
+    }
+
+    @DeleteMapping("/delete/sestavine/{sestavinaId}")
+    public void deleteSestavina(@PathVariable("sestavinaId") Integer sestavinaId) {
+        sestavineRepository.deleteById(sestavinaId);
+    }
+
+    @PutMapping("/update/sestavine/{sestavinaId}")
+    public Sestavina updateSestavina(@RequestBody Sestavina sestavina, @PathVariable("sestavinaId") Integer sestavinaId) {
+
+        Optional<Sestavina> sestavinaToUpdate = sestavineRepository.findById(sestavinaId);
+
+        return sestavinaToUpdate.map(s -> {
+            s.setRecept(s.getRecept());
+            s.setEnotaKolicine(sestavina.getEnotaKolicine());
+            s.setIme(sestavina.getIme());
+            s.setKolicina(sestavina.getKolicina());
+            return sestavineRepository.save(s);
+        }).orElseGet(() -> {
+            sestavina.setSestavinaId(sestavinaId);
+            sestavina.setCreated(LocalDate.now());
+            return sestavineRepository.save(sestavina);
         });
     }
 
